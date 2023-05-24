87a3898242675364b53d328517276d550de1680c
aneesh mohanan <aneesh.mohanan@vonnue.com>
2023-05-24 21:33:09 +0530

    letter occurence

---
+++ pg005/script.js
@@ -0,0 +1,14 @@
+function letterFrequency(str) {
+  let obj = {};
+
+  for (let i = 0; i < str.length; i++) {
+    if (obj[str[i]]) {
+      obj[str[i]] = obj[str[i]] + 1
+    } else {
+      obj[str[i]] = 1;
+    }
+  }
+  return obj
+}
+
+console.log(letterFrequency('programming'))
