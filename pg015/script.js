b2f1279340836279bb3efdc45587aab0a538b64d
aneesh mohanan <aneesh.mohanan@vonnue.com>
2023-06-03 20:03:22 +0530

    reverse string in sentence

--- pg015/script.js
+++ pg015/script.js
@@ -1,14 +1,52 @@
 function reverseString(strList) {
   let word = "";
-  let reversedWord = "";
-  for (let i = 0; i < strList.length; i++) {
-    word += strList[i];
-    if(strList[i] === ' '){
-        reversedWord = reverseWord(word);
+  let wordArr = [];
+  let reversedWordSentence = "";
+
+  // for (let i = 0; i < strList.length; i++) {
+  //   if((strList[i].charCodeAt(0) >= 65 && strList[i].charCodeAt(0) <= 122) && i !== strList.length){
+  //     word += strList[i];
+  //   }
+  //   else {
+  //     // wordArr.push(reverseWord(word))
+  //     // if(strList[i] === " " && i !== strList.length - 1){
+  //     //   wordArr.push(strList[i])
+  //     // }
+  //     // word = ''
+  //     wordArr.push(word)
+
+  //   }
+
+  //   // else if(strList[i] === " "){
+  //   //   console.log(word)
+  //   //   word = reverseWord(word) + " "
+  //   //   wordArr.push(word)
+  //   // }
+
+  //   // else if(strList[i] === " " || i !== strList.length){
+  //   //   console.log(word)
+  //   //   wordArr.push(reverseWord(word))
+  //   //   wordArr.push(' ')
+  //   // }
+  //   // console.log(wordArr)
+  // }
+
+  for (let i = 0; i <= strList.length; i++) {
+    if (strList[i] !== " " && i !== strList.length) {
+      word += strList[i];
+    } else {
+      wordArr.push(reverseWord(word));
+      if (i !== strList.length) {
+        wordArr.push(" ");
+      }
+      word = "";
     }
   }
-  
-  return reversedWord
+
+  for (let i = 0; i < wordArr.length; i++) {
+    reversedWordSentence += wordArr[i];
+  }
+  return reversedWordSentence;
 }
 
 function reverseWord(str) {
@@ -23,5 +61,5 @@ function reverseWord(str) {
   return letter;
 }
 
-let ans = reverseString("Programmers are awesome")
-console.log(ans)
+let ans = reverseString("Hello world");
+console.log(ans);
