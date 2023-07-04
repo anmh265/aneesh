4960490aaf9734e88fa75184879dc9c8a57df8ea
aneesh mohanan <aneesh.mohanan@vonnue.com>
2023-07-04 22:34:44 +0530

    test case for prime number written

--- pg001/script.js
+++ pg001/script.js
@@ -1,38 +1,38 @@
 function isPrime(num) {
-  num = returnNum(num)
-  let count = 0;
-  let flag = true
-  if (num === 1) {
-    return `Neither prime neither composite`;
-  }
+  let primeFound = true
+  if(typeof num !== 'number') return false
+  if(Math.round(num) !== num) return false
 
-  for (let i = 2; i <= num; i++) {
+  for (let i = 2; i <= num / 2; i++) {
     if (num % i === 0) {
-      count++;
+      primeFound = false
+      break
     }
   }
-  console.log(count)
 
-  if(count > 1){
-    return `Not Prime`
-  } else {
-    return `Is Prime`
-  }
+  return primeFound 
 }
 
-function returnNum(x){
-  if(x === '0') return 0
-  if(x === '1') return 1
-  if(x === '2') return 2
-  if(x === '3') return 3
-  if(x === '4') return 4
-  if(x === '5') return 5
-  if(x === '6') return 6
-  if(x === '7') return 7
-  if(x === '8') return 8
-  if(x === '9') return 9
+let testCase = [
+  {value: 2, expected: true},
+  {value: 5, expected: true},
+  {value: 6, expected: false},
+  {value: 6.6, expected: true},
+  {value: 6.0, expected: false},
+  {value: 'hello', expected: false},
+  {value: -2, expected: false},
+  {value: 1000000000, expected: false}
+]
 
-  return x
+function testCase_isPrime(tstCs){
+  tstCs.forEach((tst, i) => {
+    let got = isPrime(tst.value)
+    if(got === tst.expected){
+      console.log(`Test for ${i} passed, got = ${got}, expected = ${tst.expected}, value = ${tst.value}`)
+    } else {
+      console.log(`Test for ${i} failed, got = ${got}, expected = ${tst.expected}, value = ${tst.value}`)
+    }
+  })
 }
 
-console.log(isPrime(5))
+testCase_isPrime(testCase)
