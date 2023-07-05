85ce8bb300246976f5dea08c46fb3885ea6cee1a
aneesh mohanan <aneesh.mohanan@vonnue.com>
2023-07-05 18:57:12 +0530

    refactored and test case written

--- pg002/script.js
+++ pg002/script.js
@@ -1,12 +1,67 @@
-function wordCount(string){
-    let count = 0
-    for(let i = 0; i < string.length; i++){
-        if(string[i] === ' '){
-            count++
-        }
+function findWordCount(str) {
+  let count = 0;
+
+  if (typeof str !== "string") return -1;
+  if (str === "") {
+    return 0;
+  }
+
+  let startPos = findStartEndPositon(str, "start");
+  let endPos = findStartEndPositon(str);
+
+  if (startPos === endPos) return 0;
+
+  for (let i = startPos; i < endPos; i++) {
+    if (str[i] === " ") {
+      count++;
     }
-    return count + 1
+  }
+  return count + 1;
 }
 
-let words = wordCount('hello world')
-console.log(words)
+function findStartEndPositon(str, pos) {
+  let index = 0;
+  for (let i = 0; i < str.length; i++) {
+    if (
+      (str[i].charCodeAt(0) >= 65 && str[i].charCodeAt(0) <= 90) ||
+      (str[i].charCodeAt(0) >= 97 && str[i].charCodeAt(0) <= 122)
+    ) {
+      index = i;
+      if (pos === "start") {
+        break;
+      }
+    }
+  }
+  return index;
+}
+
+const testCase = [
+  { value: "hello all", expected: 2 },
+  { value: "  Hi all", expected: 2 },
+  { value: "lets go out        ", expected: 3 },
+  { value: 23, expected: -1 },
+  { value: " ", expected: 0 },
+  { value: "", expected: 0 },
+];
+
+function testCase_findWordCount(tstCs) {
+  tstCs.forEach((tst, i) => {
+    let wordCount = findWordCount(tst.value);
+    if (wordCount === tst.expected) {
+      console.log(
+        `Test ${i} passed for value: ${tst.value}, wordCount: ${wordCount}, expected: ${tst.expected}`
+      );
+    } else {
+      console.log(
+        `Test ${i} failed for value: ${tst.value}, wordCount: ${wordCount}, expected: ${tst.expected}`
+      );
+    }
+  });
+}
+
+testCase_findWordCount(testCase);
+
+// "hello world" - 2;
+// "world" - 1;
+// "" - 0;
+// 1234 - -1;
