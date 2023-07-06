fba69b9ff394fd226574a90f047deb3701fb5db8
aneesh mohanan <aneesh.mohanan@vonnue.com>
2023-07-06 22:02:21 +0530

    else condition for fail condition

--- pg003/script.js
+++ pg003/script.js
@@ -8,9 +8,34 @@ function returnEvenNumberList(arr) {
     }
     i++;
   }
-  return myArr
+  return myArr;
 }
 
-let arr = [1,3,4,8,9, 10]
-let ans = returnEvenNumberList(arr)
-console.log(ans)
+// let arr = [-2, 6, -4, 3];
+// let ans = returnEvenNumberList(arr);
+// console.log(ans);
+
+const testCase = [
+  { value: [2, 1, 4, 5, 6, 9], expected: [2, 4, 6] },
+  { value: [-2, 6, -4, 3], expected: [-2, 6, -4] },
+];
+
+function testCase_returnEvenNumberList(tstCs) {
+  tstCs.forEach((tst) => {
+    let got = returnEvenNumberList(tst.value);
+
+    for (let i = 0; i < got.length; i++) {
+      if (got[i] === tst.expected[i]) {
+        console.log(
+          `Test for ${i} passed, got: ${got}, expected: ${tst.expected}`
+        );
+      } else {
+        console.log(
+          `Test for ${i} failed, got: ${got}, expected: ${tst.expected}`
+        );
+      }
+    }
+  });
+}
+
+testCase_returnEvenNumberList(testCase);
