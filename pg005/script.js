103e14b6ab76d7d07b9fb69043b9e0fc5e46bc63
aneesh mohanan <aneesh.mohanan@vonnue.com>
2023-07-13 20:12:20 +0530

    refactor: test case written

--- pg005/script.js
+++ pg005/script.js
@@ -1,15 +1,25 @@
 function letterFrequency(str) {
   let letterFreqObj = {};
-  str = str.toLowerCase()
 
   for (let i = 0; i < str.length; i++) {
+    str[i] = str[i].toLowerCase();
     if (letterFreqObj[str[i]]) {
-      letterFreqObj[str[i]] = letterFreqObj[str[i]] + 1
+      letterFreqObj[str[i]] = letterFreqObj[str[i]] + 1;
     } else {
       letterFreqObj[str[i]] = 1;
     }
   }
-  return letterFreqObj
+  return letterFreqObj;
 }
 
-console.log(letterFrequency('apple'))
+const testCases = [{ value: "hello", expected: { h: 1, e: 1, l: 2, o: 1 } }];
+
+function testCase_letterFrequency(tstCs) {
+  tstCs.forEach((tst, idx) => {
+    const result = letterFrequency(tst.value);
+    console.log(Object.values(result));
+  });
+}
+
+// console.log(letterFrequency("apple"));
+testCase_letterFrequency(testCases);
