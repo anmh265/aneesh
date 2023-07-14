5171118d281a1d7c1986175cd653971cd15bc456
aneesh mohanan <aneesh.mohanan@vonnue.com>
2023-07-14 22:52:54 +0530

    testcase written

--- pg007/script.js
+++ pg007/script.js
@@ -2,7 +2,17 @@ function findLongestWord(strList) {
   let currStr = "";
   let longestStr = "";
 
+  if (typeof strList !== "string") return "";
+
   for (let i = 0; i <= strList.length; i++) {
+    console.log(strList[i].charCodeAt(0));
+    if (
+      (strList[i].charCodeAt(0) < 65 && strList[i].charCodeAt(0) > 90) ||
+      (strList[i].charCodeAt(0) < 97 && strList[i].charCodeAt(0) > 122)
+    ) {
+      return "";
+    }
+
     if (strList[i] !== " " && i !== strList.length) {
       currStr += strList[i];
     } else {
@@ -16,4 +26,22 @@ function findLongestWord(strList) {
   return longestStr;
 }
 
-console.log(findLongestWord("Hello world of programming"));
+const testcase = [
+  { value: "Helo world", expected: "world" },
+  // { value: "", expected: "" },
+  // { value: 234, expected: "" },
+];
+
+function testCase_findLongestWord(tstCs) {
+  tstCs.forEach((tst, idx) => {
+    const result = findLongestWord(tst.value);
+    console.log(
+      `Test ${
+        result === tst.expected ? "passed" : "failed"
+      } for expected value ${tst.expected}, result ${result}`
+    );
+  });
+}
+
+// console.log(findLongestWord("Hello world of programming"));
+console.log(testCase_findLongestWord(testcase));
