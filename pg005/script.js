960af14fdefcc83e81b7589253f6eccfa90eae75
aneesh mohanan <aneesh.mohanan@vonnue.com>
2023-07-17 21:18:41 +0530

    test function to compare 2 objects written

--- pg005/script.js
+++ pg005/script.js
@@ -12,14 +12,30 @@ function letterFrequency(str) {
   return letterFreqObj;
 }
 
-const testCases = [{ value: "hello", expected: { h: 1, e: 1, l: 2, o: 1 } }];
+const testCases = [
+  { value: "hello", expected: { h: 1, e: 1, l: 2, o: 1 } },
+  { value: "World", expected: { W: 1, o: 1, r: 1, l: 1, d: 1 } },
+];
 
 function testCase_letterFrequency(tstCs) {
   tstCs.forEach((tst, idx) => {
+    let count = 0;
     const result = letterFrequency(tst.value);
-    console.log(Object.values(result));
+    if (Object.keys(result).length === Object.keys(tst.expected).length) {
+      for (let property in tst.expected) {
+        if (result[property] === tst.expected[property]) {
+          count++;
+        }
+      }
+      console.log(
+        `Test for ${idx} ${
+          count === Object.keys(tst.expected).length ? "passed" : "failed"
+        }, expected: ${JSON.stringify(tst.expected)}, result: ${JSON.stringify(
+          result
+        )}`
+      );
+    }
   });
 }
 
-// console.log(letterFrequency("apple"));
 testCase_letterFrequency(testCases);
