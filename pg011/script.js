d33f0504a8b6f434c3115b018a6b57e914bf999d
aneesh mohanan <aneesh.mohanan@vonnue.com>
2023-07-20 21:57:35 +0530

    test case written

--- pg011/script.js
+++ pg011/script.js
@@ -1,13 +1,31 @@
 function checkPerfectSquare(num) {
-    if(num === 1){
-        return true
-    }
+  if (num === 1) {
+    return true;
+  }
   for (let i = 2; i <= Math.floor(num / 2); i++) {
     if (i * i === num) {
       return true;
     }
   }
-  return false
+  return false;
 }
 
-console.log(checkPerfectSquare(4));
+const testCases = [
+  { value: 4, expected: true },
+  { value: 9, expected: true },
+  { value: 5, expected: false },
+];
+
+function testCases_checkPerfectSquare(tstCs) {
+  tstCs.forEach((tst, idx) => {
+    const result = checkPerfectSquare(tst.value);
+    console.log(
+      `Test case for ${idx} ${
+        result === tst.expected ? "passed" : "failed"
+      }, expected: ${tst.expected} result ${result}`
+    );
+  });
+}
+
+// console.log(checkPerfectSquare(4));
+testCases_checkPerfectSquare(testCases);
