ee119fb984576da7740cf9c4c22d92d65b446ad2
aneesh mohanan <aneesh.mohanan@vonnue.com>
2023-07-30 23:51:01 +0530

    updated

--- pg005/script.js
+++ pg005/script.js
@@ -17,6 +17,7 @@ const testCases = [
   { value: "hello", expected: { h: 1, e: 1, l: 2, o: 1 } },
   { value: "World", expected: { W: 1, o: 1, r: 1, l: 1, d: 1 } },
   { value: 123, expected: {} },
+  { value: " ", expected: {} },
 ];
 
 function testCase_letterFrequency(tstCs) {
